fb8de7dc63a91009870a80fbfc3128b2491c7da6
Angel <angelwang3011@gmail.com>
2024-08-08 11:33:29 +0800

    Updates

--- assets/photo2_0007_list02-CWk8E7Gz.js
+++
@@ -1,1 +0,0 @@
-const s="/images/photo2_0008_list01.png",o="/images/photo2_0007_list02.png";export{s as _,o as a};
---
+++ assets/photo2_0007_list02-TKNZlVtd.js
@@ -0,0 +1,1 @@
+const s="/kuma/images/photo2_0008_list01.png",o="/kuma/images/photo2_0007_list02.png";export{s as _,o as a};
